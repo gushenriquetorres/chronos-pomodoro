639d7f1f81790902d6445446fa15021aaa7e959f
Gustavo Henrique <gustavohenrique@pactosolucoes.com.br>
2025-09-30 08:07:07 -0300

    feat: add Heading style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import './styles/themes.css'
 import './styles/global.css'
 import { Heading } from './components/Heading'
+import { TimerIcon } from 'lucide-react'
 
 export function App() {
-    return (
-        <>
-            <Heading />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae harum modi, illum in explicabo asperiores? Cum vitae voluptas expedita obcaecati possimus harum eligendi unde, ut natus earum illum consequatur est!</p>
-        </>
-    )
+  return (
+    <>
+      <Heading>
+        Olá mundo
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae harum modi, illum in explicabo asperiores? Cum vitae voluptas expedita obcaecati possimus harum eligendi unde, ut natus earum illum consequatur est!</p>
+    </>
+  )
 }
